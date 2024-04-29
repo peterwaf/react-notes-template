f26270d8eba406ce3aeb9a93b24398c8da960749
peterwaf <peterwafulah@gmail.com>
2024-04-29 22:24:15 +0300

    set up

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid">
+        <header className="row header">
+          <div className="col">
+            <h1 className="text-start">Keeper</h1>
+          </div>
+        </header>
+        <div className="row content">
+          <div className="col-md-3 col-sm-12 mb-3 mt-3">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">This is the note title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
 }
